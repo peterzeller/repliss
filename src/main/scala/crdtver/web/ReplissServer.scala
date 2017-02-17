462f0cd7e1de8285dff6bd71eab557a61c3e1101
Peter Zeller <p_zeller@cs.uni-kl.de>
2017-02-17 18:22:05 +0100

    adding new examples to web interface

--- src/main/scala/crdtver/web/ReplissServer.scala
+++ src/main/scala/crdtver/web/ReplissServer.scala
@@ -30,8 +30,6 @@ object ReplissServer extends ServerApp {
       .mountService(staticFiles("/META-INF/resources/webjars"), "/webjars/")
       .mountService(service, "/api")
       .mountService(indexPage, "/")
-      .mountService(test, "/test")
-      .mountService(test2, "/test2")
       .start
   }
 
@@ -41,48 +39,6 @@ object ReplissServer extends ServerApp {
     case request@GET -> Root / "webjars" / "ace" / "01.08.2014" / "src-noconflict" / "mode-repliss.js" =>
       StaticFile.fromResource("/js/mode-repliss.js").map(Task.now).get
 
-  }
-
-  private val test = HttpService {
-    case request@GET -> Root =>
-      import scala.concurrent.duration._
-      import scalaz.stream.time
-      implicit val S = scalaz.concurrent.Strategy.DefaultTimeoutScheduler
-      val seconds = time.awakeEvery(1.seconds)
-      val task: Process[Task, String] = seconds.take(10).zipWithIndex.map { case (d, i) => s"$i\n" }
-      Ok.apply(task)(debugEncode)
-  }
-
-  private val test2 = HttpService {
-    case request@GET -> Root =>
-      val S: ScheduledExecutorService = scalaz.concurrent.Strategy.DefaultTimeoutScheduler
-      val strategy: Strategy = Strategy.Executor(S)
-//      import scala.concurrent.ExecutionContext.Implicits.global
-      val responseQueue: Queue[String] = async.unboundedQueue[String](strategy)
-      val task: Process[Task, String] = responseQueue.dequeue
-
-      Future {
-
-        for (i <- 0 to 10) {
-          println(s"adding $i")
-          responseQueue.enqueueOne(s"hi $i<br />\n")
-          Thread.sleep(100)
-        }
-        //        responseQueue.fail(new RuntimeException("rofl"))
-        responseQueue.close
-      } (scala.concurrent.ExecutionContext.Implicits.global)
-      println("running")
-
-
-      val task2: Task[Unit] = task.map(s => {
-        println(s"reading $s")
-      }).run
-
-      println("running2")
-      val res = task2.get.run
-
-      println("returning")
-      Ok.apply(task)(debugEncode)
   }
 
   def debugEncode[A](implicit W: EntityEncoder[A]): EntityEncoder[Process[Task, A]] =
@@ -154,7 +110,9 @@ object ReplissServer extends ServerApp {
       ReplissExample("Userbase (missing transaction)", "userbase_fail1.rpls"),
       ReplissExample("Userbase (wrong CRDT)", "userbase_fail2.rpls"),
       ReplissExample("Friends", "friends.rpls"),
-      ReplissExample("Friends 2", "friends2.rpls")
+      ReplissExample("Friends 2", "friends2.rpls"),
+      ReplissExample("Singleton set (bug)", "singleton_set.rpls"),
+      ReplissExample("Active view (bug)", "active_view.rpls")
     )
 
     import org.json4s._
